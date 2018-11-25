7d3b742a0b2eac9b882e15c1b055fbe6325b652e
prabawa30 <45327551+prabawa30@users.noreply.github.com>
2018-11-25 16:08:55 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3jRsL2CobTLituxfX5qfXbEk4LMfaqzm3qehXqwzYCHnd6vuo6lv7V2la2jra6KZM3XjZSKo7";
+$meid = "34925cecfeb90301";
+$os="android";
+$sign="4cfc005284d0aacbf6e7c19180ed0e8c1941fdbb58943a0152c79e5de3f51623";
